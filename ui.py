2e3a5c8191c8bda64afbcbebeb80d5b419a025bc
V Sree Kirthana <sreekeerthana64@gmail.com>
2026-07-18 16:26:12 +0530

    Increase font size in UI to 60px

--- ui.py
+++ ui.py
@@ -6,7 +6,7 @@ def render_aurora_landing_page(auth_url: str) -> None:
     """
     <style>
     p {
-        font-size: 25px;  
+        font-size: 60px;  
         line-height: 1.6;
      }
      ul li,
