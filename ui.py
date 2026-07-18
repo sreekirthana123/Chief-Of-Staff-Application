66b96704f8d0613dc3815252e957f99c564b9aa3
V Sree Kirthana <sreekeerthana64@gmail.com>
2026-07-18 16:37:16 +0530

    Increase font size for list items in ui.py

--- ui.py
+++ ui.py
@@ -11,7 +11,7 @@ def render_aurora_landing_page(auth_url: str) -> None:
       .element-container > div,
       ul li,  /* Unordered lists */
        ol li {  /* Ordered lists */ 
-          font-size: 22px !important;
+          font-size: 28px !important;
        }
         /* The Aurora Background */
         .stApp {
