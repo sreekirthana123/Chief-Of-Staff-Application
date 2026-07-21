e985f35bdde4be0f7570feb8114c0f89b8819207
V Sree Kirthana <sreekeerthana64@gmail.com>
2026-07-21 19:08:04 +0530

    Decrease font size in UI styles

--- ui.py
+++ ui.py
@@ -32,7 +32,7 @@ def apply_app_theme() -> None:
         color: #3A2A1E !important;
     }
     .stApp p, .stApp span, .stApp label, .stApp li {
-        font-size: 2.2rem !important;
+        font-size: 2.0rem !important;
         line-height: 1.7 !important;
         color: #3A2A1E;
     }
